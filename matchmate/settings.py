cd5938fc00c8de0bf0097f51593be62c7f97138c
ALI ABBAS <aliabbas541258@gmail.com>
2026-01-28 21:36:59 +0500

    Update settings configuration

--- matchmate/settings.py
+++ matchmate/settings.py
@@ -57,10 +57,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+     'corsheaders',
     'matching_app.apps.MatchingAppConfig',
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
